9cbf8c7b7d504d9f8e707618a5708e7adb0d5a23
Rafel <ralfrr55@gmail.com>
2022-12-26 10:38:35 -0300

    templates e menu de navegação

---
+++ java-web2/src/main/java/br/com/javaweb/java_web2/bean/CarroBean.java
@@ -0,0 +1,45 @@
+package br.com.javaweb.java_web2.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.javaweb.java_web2.entity.Carro;
+
+@ManagedBean
+@SessionScoped
+public class CarroBean {
+	
+	private Carro carro = new Carro();
+	
+	private List<Carro> carros = new ArrayList<>();
+	
+	public void adicionar() {
+		
+		carros.add(carro);
+		carro =  new Carro();
+		
+	}
+
+	public Carro getCarro() {
+		return carro;
+	}
+
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+
+	public List<Carro> getCarros() {
+		return carros;
+	}
+
+	public void setCarros(List<Carro> carros) {
+		this.carros = carros;
+	}
+	
+	
+	
+
+}
---
+++ java-web2/src/main/java/br/com/javaweb/java_web2/entity/Carro.java
@@ -0,0 +1,37 @@
+package br.com.javaweb.java_web2.entity;
+
+public class Carro {
+	
+	private String modelo;
+	private String fabricante;
+	private String cor;
+	private String ano;
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getAno() {
+		return ano;
+	}
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	
+	
+	
+
+}
